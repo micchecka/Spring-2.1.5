8076220236586b7e63b23821d1c602663651b1d0
micchecka <micchecka@mail.ru>
2023-03-26 16:05:20 +0300

    решено

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,15 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+
+    @Autowired
+    @Qualifier("death")
+    private Deth8 deth;
 
     @Override
     public String toString() {
